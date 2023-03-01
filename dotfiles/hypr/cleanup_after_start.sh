7c9344730986cda1a64b9d754db139207f4324b4
ainchentmew2 <nvformella@gmail.com>
2023-03-01 13:38:33 -0600

    Modified cleanup script

--- dotfiles/hypr/cleanup_after_start.sh
+++ dotfiles/hypr/cleanup_after_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sleep 3
-hyprctl keyword windowrule "workspace 1,kitty"
-hyprctl dispatch exec "kitty"
 sleep 10
 hyprctl keyword windowrule "workspace unset,firefox"
 hyprctl keyword windowrule "workspace unset,kitty"

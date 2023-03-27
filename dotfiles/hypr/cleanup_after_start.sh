0a8879b7d1b73aa8ea7f058b024fe2adcbe69e52
ainchentmew2 <nvformella@gmail.com>
2023-03-27 15:55:15 -0500

    Better startup

--- dotfiles/hypr/cleanup_after_start.sh
+++ dotfiles/hypr/cleanup_after_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sleep $1
-swww img $(find ~/.config/hypr/wallpapers/ -type f | shuf -n 1) &
 hyprctl keyword windowrule "workspace unset,firefox"
 sleep 1
 hyprctl keyword windowrule "workspace unset,kitty"
